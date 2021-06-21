d4d739d0a787fd04edcfc2a8a16eba9d6bb46a56
woo972 <wzak9720>
2021-06-21 11:00:14 +0900

    add configserver

--- src/main/java/com/wowls/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/wowls/apigateway/ApiGatewayApplication.java
@@ -1,7 +1,13 @@
 package com.wowls.apigateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTrace;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -10,4 +16,8 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	@Bean
+	public HttpTraceRepository httpTrace(){
+		return new InMemoryHttpTraceRepository();
+	}
 }
--- src/main/java/com/wowls/apigateway/configuration/LoggingFilter.java
+++ src/main/java/com/wowls/apigateway/configuration/LoggingFilter.java
@@ -1,11 +1,14 @@
 package com.wowls.apigateway.configuration;
 
+import com.netflix.discovery.converters.Auto;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.core.Ordered;
+import org.springframework.core.env.Environment;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
@@ -17,6 +20,10 @@ public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Co
     public LoggingFilter() {
         super(Config.class);
     }
+
+    @Autowired
+    private Environment env;
+
 
     @Data
     public static class Config {
@@ -30,6 +37,7 @@ public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Co
         return new OrderedGatewayFilter((exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
             ServerHttpResponse response = exchange.getResponse();
+            log.info("token:{}", env.getProperty("token.secret"));
             if (config.preFilter) {
                 log.info("pre logging filter : request id : {}", request.getId());
             }
